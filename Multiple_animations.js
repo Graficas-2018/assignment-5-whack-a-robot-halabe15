60b1e26d7505370c00c74ee860a684f67f116785
halabe15 <halabe15@gmail.com>
2018-10-11 16:22:41 -0500

    avances

--- Multiple_animations.js
+++ Multiple_animations.js
@@ -1,96 +1,75 @@
+var SHADOW_MAP_WIDTH = 2048, SHADOW_MAP_HEIGHT = 2048, TIME = 5;
+
 var renderer = null,
 scene = null,
 camera = null,
 root = null,
 robot_idle = null,
 robot_attack = null,
-flamingo = null,
-stork = null,
 group = null,
 orbitControls = null,
 raycaster = null;
 
 var robot_run = null;
-var robot_group = {};
+var robot_running = {};
 var intersects = [];
 var deadAnimator;
-var interval = null;
 var mouse = new THREE.Vector2(), INTERSECTED, CLICKED;
 var duration = 20000; // ms
 var currentTime = Date.now();
-
 var animation = "run";
-
 var score = 0;
-var time = 0;
+var time = TIME;
+var ambientLight = null;
+var mapUrl = "../images/grass.jpg";
+var game_over = false;
 
-function changeAnimation(animation_text)
-{
-    animation = animation_text;
 
-    if(animation == "dead")
-    {
-        createDeadAnimation();
+function run() {
+  if (!game_over) {
+    requestAnimationFrame(function() { run(); });
+
+        // Render the scene
+        renderer.render( scene, camera );
+
+        // Spin the cube for next frame
+        animate();
+
+        // Update the camera controller
+        orbitControls.update();
+        if (time == 0)
+          gameover();
+  }
+
+}
+
+function animate() {
+
+    var now = Date.now();
+    var deltat = now - currentTime;
+    currentTime = now;
+
+    if(robot_run){
+      robot_run.update(deltat * 0.001);
+    }
+
+    KF.update();
+
+    for (var i in robot_running.children) {
+      if (robot_running.children[i].die != true) {
+        robot_running.children[i].position.z += 0.3;
+      } else {
+        if(!robot_running.children[i].deadAnimator.running)
+          robot_running.remove(robot_running.children[i]);
+      }
+      if (robot_running.children[i].position.z > 100) {
+        robot_running.remove(robot_running.children[i]);
+        addScore(-1);
+      }
     }
 }
 
-function createDeadAnimation()
-{
-    // position animation
-    if (deadAnimator)
-        deadAnimator.stop();
-
-    // group.position.set(0, 0, 0);
-    // group.rotation.set(0, 0, 0);
-
-    if (animation == "dead")
-    {
-        deadAnimator = new KF.KeyFrameAnimator;
-        deadAnimator.init({
-            interps:
-                [
-                    {
-                        keys:[0, .25, .5, 1],
-                        values:[
-                                { x : 0 },
-                                { x : -Math.PI /8 },
-                                { x : -Math.PI /4},
-                                { x : -Math.PI /2},
-                                ],
-                        target:robot_idle.rotation
-                    },
-                    {
-                        keys:[0, .25, .5, 1],
-                        values:[
-                                { y : -4 },
-                                { y : -3 },
-                                { y : -2},
-                                { y : -1},
-                                ],
-                        target:robot_idle.position
-                    },
-                ],
-            loop: false,
-            duration:1000,
-            // easing:TWEEN.Easing.Bounce.InOut,
-        });
-        deadAnimator.start();
-
-    }
-}
-
-function createRobots(){
-  if (robot_group.children.length > 10)
-    return;
-  var object = cloneFbx(robot_idle);
-  object.position.x = Math.random() * (45 + 45) - 45;
-  object.position.z = -100;
-  robot_run.clipAction( object.animations[ 0 ], object ).play();
-  robot_group.add(object);
-}
-
-function loadFBX()
-{
+function loadFBX(){
     var loader = new THREE.FBXLoader();
     loader.load( '../models/Robot/robot_run.fbx', function ( object )
     {
@@ -105,82 +84,72 @@ function loadFBX()
             }
         } );
         robot_idle = object;
-        robot_group.add( robot_idle );
+        robot_running.add( robot_idle );
 
-        createDeadAnimation();
+        // createDeadAnimation();
 
         robot_run.clipAction( object.animations[ 0 ], robot_idle ).play();
     } );
 }
 
-function animate() {
-
-    var now = Date.now();
-    var deltat = now - currentTime;
-    currentTime = now;
-
-    if(robot_run){
-      robot_run.update(deltat * 0.001);
-    }
-
-
-    switch (animation) {
-      case "dead":
-        KF.update();
-        // robot_idle.position.y -= 0.04;
-        // robot_idle.opacity += 1;
-      break;
-
-      case "run":
-        // robot_idle.position.z += 0.3;
-        // console.log(robot_group);
-        for (var i in robot_group.children) {
-          robot_group.children[i].position.z += 0.3;
-        }
-      break;
-      default:
-
-    }
-
-    for (var i in robot_group.children) {
-      if (robot_group.children[i].position.z > 100) {
-        robot_group.remove(robot_group.children[i]);
-      }
-    }
-
+function createRobots(){
+  if (robot_running.children.length > 10)
+    return;
+  var object = cloneFbx(robot_idle);
+  object.position.x = Math.random() * (45 + 45) - 45;
+  object.position.y = -4;
+  object.position.z = -100;
+  object.rotation.x = 0;
+  robot_run.clipAction( object.animations[ 0 ], object ).play();
+  robot_running.add(object);
 }
 
-function run() {
-    requestAnimationFrame(function() { run(); });
+function createDeadAnimation(object){
+    // position animation
+    // if (deadAnimator)
+    //     deadAnimator.stop();
 
-        // Render the scene
-        renderer.render( scene, camera );
+    // group.position.set(0, 0, 0);
+    // group.rotation.set(0, 0, 0);
 
-        // Spin the cube for next frame
-        animate();
+    // if (animation == "dead")
+    // {
+        object.deadAnimator = new KF.KeyFrameAnimator;
+        object.deadAnimator.init({
+            interps:
+                [
+                    {
+                        keys:[0, .25, .5, 1],
+                        values:[
+                                { x : 0 },
+                                { x : -Math.PI /8 },
+                                { x : -Math.PI /4},
+                                { x : -Math.PI /2},
+                                ],
+                        target:object.rotation
+                    },
+                    {
+                        keys:[0, .25, .5, 1],
+                        values:[
+                                { y : -4 },
+                                { y : -3 },
+                                { y : -2},
+                                { y : -1},
+                                ],
+                        target:object.position
+                    },
+                ],
+            loop: false,
+            duration:1000,
+            // easing:TWEEN.Easing.Bounce.InOut,
+        });
+        object.deadAnimator.start();
+        // object.parent.remove(object);
 
-        // Update the camera controller
-        orbitControls.update();
+    // }
 }
 
-function setLightColor(light, r, g, b)
-{
-    r /= 255;
-    g /= 255;
-    b /= 255;
-
-    light.color.setRGB(r, g, b);
-}
-
-var directionalLight = null;
-var spotLight = null;
-var ambientLight = null;
-var mapUrl = "../images/grass.jpg";
-
-var SHADOW_MAP_WIDTH = 2048, SHADOW_MAP_HEIGHT = 2048;
-
-function createScene(canvas) {
-
+function createScene(canvas){
     // Create the Three.js renderer and attach it to our canvas
     renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
 
@@ -205,28 +174,18 @@ function createScene(canvas) {
     // Create a group to hold all the objects
     root = new THREE.Object3D;
 
-    // spotLight = new THREE.SpotLight (0xffffff);
-    // spotLight.position.set(-30, 8, -10);
-    // spotLight.target.position.set(-2, 0, -2);
-    // root.add(spotLight);
 
-    // spotLight.castShadow = true;
     //
-    // spotLight.shadow.camera.near = 1;
-    // spotLight.shadow.camera.far = 200;
-    // spotLight.shadow.camera.fov = 45;
     //
-    // spotLight.shadow.mapSize.width = SHADOW_MAP_WIDTH;
-    // spotLight.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
 
     ambientLight = new THREE.AmbientLight ( 0xffffff );
     root.add(ambientLight);
 
     // Create a group to hold the objects
     group = new THREE.Object3D;
-    robot_group = new THREE.Object3D;
+    robot_running = new THREE.Object3D;
     root.add(group);
-    root.add(robot_group);
+    root.add(robot_running);
 
     // Create the objects
     loadFBX();
@@ -260,8 +219,7 @@ function createScene(canvas) {
 
 }
 
-function onDocumentMouseDown(event)
-{
+function onDocumentMouseDown(event){
     event.preventDefault();
     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
@@ -269,27 +227,79 @@ function onDocumentMouseDown(event)
     // find intersections
     raycaster.setFromCamera( mouse, camera );
 
-    for (var i in robot_group.children) {
-      intersects.push(raycaster.intersectObjects( robot_group.children[i].children ));
+    for (var i in robot_running.children) {
+      intersects.push(raycaster.intersectObjects( robot_running.children[i].children ));
     }
 
     for (var i in intersects) {
       if ( intersects[i].length > 0 ){
         CLICKED = intersects[i][0].object;
-        robot_group.remove(robot_group.children[i]);
-        addScore();
+        // robot_running.remove(robot_running[i]);
+        // createDeadAnimation(i);
+        // CLICKED.parent.remove(CLICKED);
+        CLICKED.parent.die = true;
+        createDeadAnimation(CLICKED.parent);
+        addScore(1);
       } else
         CLICKED = null;
     }
 
+    intersects = [];
+
 }
 
-function addScore(){
-  score++;
+function gameover(){
+  game_over = true;
+  $('#container').hide();
+  $('#menu').show();
+}
+
+function start(){
+  $('#menu').hide();
+  $('#container').show();
+
+  time = TIME;
+  addScore(-score);
+  game_over = false;
+
+  renderer = null;
+  scene = null;
+  camera = null;
+  root = null;
+  robot_idle = null;
+  robot_attack = null;
+  group = null;
+  orbitControls = null;
+  raycaster = null;
+
+  robot_run = null;
+  robot_running = {};
+  intersects = [];
+  deadAnimator;
+  mouse = new THREE.Vector2(), INTERSECTED, CLICKED;
+  duration = 20000; // ms
+  currentTime = Date.now();
+  animation = "run";
+  score = 0;
+  time = TIME;
+  ambientLight = null;
+  mapUrl = "../images/grass.jpg";
+  game_over = false;
+  createScene();
+
+  run();
+}
+
+function addScore(num){
+  score = score + num;
+  if (score < 0)
+    score = 0;
   $('#title').text("Score: "+score);
 }
 
 function timer(){
-  time++;
+  time--;
+  if (time < 0)
+    time = 0;
   $('#animations').text("Time: "+time+" segs");
 }
